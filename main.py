25bb83bfaaa51cac0225af6fe4f5538d74934ed2
Caio Victor <77304049+caiovictorpcb@users.noreply.github.com>
2021-08-04 18:05:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 import requests as r 
 from bs4 import BeautifulSoup
 
-def pedirNome():
-    global nome
-    nome = input("Digite o nome da pessoa: ")
-    nome = nome.title()
-    nome = nome.split()
-    if len(nome) == 2:
-        nome = nome[0] + "_" + nome[1]
-        return True
-    elif len(nome) < 2:
-        print("Nome não suportado.Por favor digite o nome e sobrenome da pessoa.")
-        return False
-    else:
-        print("Nome não suportado.Por favor digite apenas o nome e sobrenome da pessoa.")
-        return False
+def pedir_nome():
+    while True:
+        nome = input("Digite o nome da pessoa: ")
+        nome = nome.title()
+        nome = nome.split()
+        if len(nome) == 2:
+            nome = nome[0] + "_" + nome[1]
+            return nome
+        else:
+            print("Digite o nome com apenas um espaço separando-os.")
+            continue
 
     
-def fazerRequisicao():
-    url = "https://pt.wikipedia.org/wiki/" + nome
-    resposta = r.get(url).text
-    soup = BeautifulSoup(resposta, 'html.parser')
-    print(soup.find("p").text)
+def fazer_requisicao():
+        url = "https://pt.wikipedia.org/wiki/" + pedir_nome()
+        resposta = r.get(url).text
+        soup = BeautifulSoup(resposta, 'html.parser')
+        print(soup.find("p").text)
 
 
-if pedirNome():
-    fazerRequisicao()
-else:
-    print("ERRO EM ALGUM LUGAR AI BIXO")
+fazer_requisicao()
+
 
